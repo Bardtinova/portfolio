6728662090d0bbf2d1d6173fe77166ff33de0933
nataliya bardtinova <bardtinova@gmail.com>
2022-12-24 22:18:20 +0200

    add mask.js

---
+++ project-JS/picture/src/js/modules/mask.js
@@ -0,0 +1,11 @@
+const mask = (selector) => {
+
+    function creatMask(event) {
+        let matrix = '+38(___) ___ __ __',
+            i = 0,
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, '');
+    }
+};
+
+export default mask;
